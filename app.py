2840df7ccd0ed3c31ecf127e7fade7f8b8d2d180
abdelrhman fawzy <abdwfawzy985%gmail.com>
2022-06-06 16:45:21 +0200

    i builded the app structure

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from helpers import apology, login_required, lookup, usd
